e97608f27a084c1417fb8465f34670adff8dd22d
YoanKirilov <92452174+YoanKirilov@users.noreply.github.com>
2022-10-26 16:42:45 +0100

    Few things

--- learn-for-fun/src/components/HomePage.js
+++ learn-for-fun/src/components/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react"; 
+import Card from 'react-bootstrap/Card';
 
 
 const HomePage = () => {
     return ( 
         <body>
             <h1>Hello World</h1>
+            <Card.Link href='./Course'>Course</Card.Link>
         </body>
     );
 }
