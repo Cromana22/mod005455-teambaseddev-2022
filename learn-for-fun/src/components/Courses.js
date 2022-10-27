cf2318872c65579a935c17a4cab003084be9dc59
CallumMcR <p110134092@student.peterborough.ac.uk>
2022-10-27 12:28:26 +0100

    Fixed Routing

--- learn-for-fun/src/components/Courses.js
+++ learn-for-fun/src/components/Courses.js
@@ -1,10 +1,11 @@
 import React from "react"; 
-
+import { Link } from "react-router-dom";
 
 const Courses = () => {
     return ( 
         <body>
-            <h1>Hello</h1>
+            
+            <h1>Hello this is courses page</h1>
         </body>
     );
 }
--- learn-for-fun/src/index.js
+++ learn-for-fun/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import HomePage from './components/HomePage';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Courses from "./components/Courses";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <HomePage />
-  </React.StrictMode>
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/courses" element={<Courses />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
