538ae96fe887fce00122abc9d664ab825c9a9838
P110136416 <p110136416@student.peterborough.ac.uk>
2022-10-29 16:01:36 +0100

    Why Us section complete

--- learn-for-fun/src/components/HomePage.jsx
+++ learn-for-fun/src/components/HomePage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import HomeCarousel from "./carousel";
-import NavBar from "./NavBar"
-import "../css/HomePage.css"
+import NavBar from "./NavBar";
+import {Button, Card} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import "../css/HomePage.css";
 
 function HomePage() {
     return (
@@ -16,10 +18,48 @@ function HomePage() {
                 <HomeCarousel />
             </div>
 
-            <div className="container section">
-                Who am i
+            <div className="container section endSection">
+                <div className="thirds">
+                    <Card style={{ width: '20rem' }} className="homeCards">
+                        <Card.Body>
+                            <Card.Title className="center">Card Title</Card.Title>
+                            <Card.Text className="justify">
+                                Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="thirds">
+                    <Card style={{ width: '20rem' }} className="homeCards">
+                        <Card.Body>
+                            <Card.Title className="center">Card Title</Card.Title>
+                            <Card.Text className="justify">
+                                Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="thirds">
+                    <Card style={{ width: '20rem' }} className="homeCards">
+                        <Card.Body>
+                            <Card.Title className="center">Card Title</Card.Title>
+                            <Card.Text className="justify">
+                                Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+
+                <div className="center section">
+                    <Link to="/register">
+                        <Button variant="primary">Subscribe</Button>
+                    </Link>
+                </div>
             </div>
+
+            <NavBar />
         </div>
     )
 }
+
 export default HomePage;
