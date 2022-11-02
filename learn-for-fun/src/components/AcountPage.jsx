7f215c510f5c4c3a6453cdd13a96d04e7f971977
P110136416 <p110136416@student.peterborough.ac.uk>
2022-11-02 15:51:53 +0000

    Rough layout of accounts page done

--- learn-for-fun/src/components/AcountPage.jsx
+++ learn-for-fun/src/components/AcountPage.jsx
@@ -1,19 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import {Button, Card} from 'react-bootstrap';
-import "../css/HomePage.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function AccountPage() {
     return (
-        <div className="account">
+        <div id="accountPage">
             <NavBar />
-
-            <div>
-                Placeholder
-            </div>
-
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <p>My Details</p>
+                        <p>My Subscriptions</p>
+                    </Col>
+                    <Col>
+                        <div id="myDetails">
+                            <p>Name: FROM DATABASE GOES HERE</p>
+                            <p>Address Line 1: FROM DATABASE GOES HERE</p>
+                            <p>Address Line 2: FROM DATABASE GOES HERE</p>
+                            <p>Town: FROM DATABASE GOES HERE</p>
+                            <p>County: FROM DATABASE GOES HERE</p>
+                            <p>Postcode: FROM DATABASE GOES HERE</p>
+                            <p>Email: FROM DATABASE GOES HERE</p>
+                        </div>
+                        <hr />
+                        <div id="subscriptionDetails">
+                            <p>Subscription Type: FROM DATABASE TYPE (COURSE NAME IF EXISTS)</p>
+                            <p>Subscribed Since: FROM DATABASE GOES HERE</p>
+                            <p>Monthly Price: £FROM DATABASE GOES HERE</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             <Footer />
         </div>
     )
