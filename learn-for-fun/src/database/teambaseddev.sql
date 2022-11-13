065b6b98ba762a9239e006df14d97b3f4c80218e
YoanKirilov <92452174+YoanKirilov@users.noreply.github.com>
2022-11-13 15:45:25 +0000

    Update teambaseddev.sql

--- learn-for-fun/src/database/teambaseddev.sql
+++ learn-for-fun/src/database/teambaseddev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2022 at 07:18 PM
+-- Generation Time: Nov 13, 2022 at 04:41 PM
 -- Server version: 5.7.17
 -- PHP Version: 5.6.30
 
@@ -43,7 +43,9 @@ CREATE TABLE `course` (
 
 INSERT INTO `course` (`courseID`, `courseName`, `youtubeURL`, `pdfFilename`, `codeFilename`, `price`) VALUES
 (1, 'Blender: Your first tutorial part 1', 'https://www.youtube.com/watch?v=TPrnSACiTJ4', 'blenderpart1.pdf', 'code.py', '5.00'),
-(2, 'Blender: Your first tutorial part 2', 'https://www.youtube.com/watch?v=TPrnSACiTJ4', 'blenderpart2.pdf', 'code.py', '5.00');
+(2, 'Blender: Your first tutorial part 2', 'https://www.youtube.com/watch?v=TPrnSACiTJ4', 'blenderpart2.pdf', 'code.py', '10.00'),
+(3, 'Blender: Your first tutorial part 3', 'https://www.youtube.com/watch?v=TPrnSACiTJ4', 'blenderpart3.pdf', 'code.py', '15.00'),
+(4, 'Blender: Your first tutorial part 4', 'https://www.youtube.com/watch?v=TPrnSACiTJ4', 'blenderpart4.pdf', 'code.py', '20.00');
 
 -- --------------------------------------------------------
 
@@ -59,6 +61,18 @@ CREATE TABLE `subscription` (
   `courseID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `subscription`
+--
+
+INSERT INTO `subscription` (`subscriptionID`, `userID`, `dateRaised`, `subscriptionType`, `courseID`) VALUES
+(1, 3, '2022-11-09', 'Game development ', 1),
+(2, 3, '2022-11-14', 'Game development; Second part ', 2),
+(3, 2, '2022-11-14', 'Game development ', 1),
+(4, 2, '2022-11-15', 'Game development; Second part ', 2),
+(5, 3, '2022-11-17', 'Game development; Third part', 3),
+(6, 1, '2022-11-13', 'Game development ', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -69,6 +83,18 @@ CREATE TABLE `subscriptiontype` (
   `subscriptionTypeID` int(11) NOT NULL,
   `price` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subscriptiontype`
+--
+
+INSERT INTO `subscriptiontype` (`subscriptionTypeID`, `price`) VALUES
+(1, '5'),
+(2, '10'),
+(3, '5'),
+(4, '10'),
+(5, '15'),
+(6, '5');
 
 -- --------------------------------------------------------
 
@@ -88,6 +114,15 @@ CREATE TABLE `user` (
   `county` varchar(50) DEFAULT NULL,
   `postcode` char(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`email`, `userID`, `password`, `firstName`, `lastName`, `address1`, `address2`, `city`, `county`, `postcode`) VALUES
+('JC@gmail.com', 1, 'Carpenter123', 'Jay', 'Carpenter', '27 North Promenade\r\nDONHEAD ST MARY', NULL, 'Shaftesbury', 'United Kingdom', 'SP7 0HW'),
+('MF@gmail.com', 2, 'Farrell123', 'Mohammad', 'Farrell', '17 Peachfield Road\r\nCEFN-COED-Y-CYMMER', NULL, 'Merthyr Tydfil', 'United Kingdom', 'CF48 8TH'),
+('AB@gmail.com', 3, 'Begum123', 'Alfie', 'Begum', '97 Park Row\r\nEGERTON', '90 Bishopgate Street\r\nSEDGEFORD', 'Egerton', 'United Kingdom', 'TN27 9TB');
 
 --
 -- Indexes for dumped tables
