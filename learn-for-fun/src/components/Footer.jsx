5eb66a160bb5c5f967892b65ca802f2d24f982a2
YoanKirilov <92452174+YoanKirilov@users.noreply.github.com>
2022-11-16 15:13:29 +0000

    Footer-content +some CSS

--- learn-for-fun/src/components/Footer.jsx
+++ learn-for-fun/src/components/Footer.jsx
@@ -1,21 +1,22 @@
-import React from "react"; 
+import React from "react";
+import './Footer.css';
+import Button from 'react-bootstrap/Button';
 
 function Footer() {
-    return ( 
-        <footer className="text-center text-dark">
+  return (
+    <footer className="text-center text-light">
 
-          <a href="#home">link</a>
-          <a href="#home">link</a>
-          <a href="#home">linkt</a>
-          <a href="#home">link</a>
+      <div className="links">
+        <Button href="https://www.facebook.com">Facebook</Button>
+        <Button href="https://www.instagram.com">Instagram</Button>
+        <Button href="https://twitter.com">Twitter</Button>
+        <Button href="https://www.youtube.com">YouTube</Button>
+      </div>
 
-          <div className="text-center text-dark p-3">
-            © 2022 Copyright:
-            <a className="text-light" href="#">Learn4Fun</a>
-          </div>
+      <h6 className="text-dark">© 2022 Copyright: Learn4Fun</h6>
 
-        </footer>
-    )
+    </footer>
+  )
 }
- 
+
 export default Footer;
