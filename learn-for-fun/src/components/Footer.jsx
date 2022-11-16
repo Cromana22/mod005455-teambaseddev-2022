8b523eabcd204fa8455b324afd0eb5f6e00ebf6b
YoanKirilov <92452174+YoanKirilov@users.noreply.github.com>
2022-11-16 15:22:11 +0000

    Little change

--- learn-for-fun/src/components/Footer.jsx
+++ learn-for-fun/src/components/Footer.jsx
@@ -5,16 +5,27 @@ import Button from 'react-bootstrap/Button';
 function Footer() {
   return (
     <footer className="text-center text-light">
+      <div className="row">
 
-      <div className="links">
-        <Button href="https://www.facebook.com">Facebook</Button>
-        <Button href="https://www.instagram.com">Instagram</Button>
-        <Button href="https://twitter.com">Twitter</Button>
-        <Button href="https://www.youtube.com">YouTube</Button>
+        <div className="col-1">
+        </div>
+
+        <div className="col-10">
+          <div className="row">
+            <div className="row border-top border-dark">
+
+              <div className="links">
+                <Button href="https://www.facebook.com">Facebook</Button>
+                <Button href="https://www.instagram.com">Instagram</Button>
+                <Button href="https://twitter.com">Twitter</Button>
+                <Button href="https://www.youtube.com">YouTube</Button>
+              </div>
+
+              <h6 className="text-dark">© 2022 Copyright: Learn4Fun</h6>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <h6 className="text-dark">© 2022 Copyright: Learn4Fun</h6>
-
     </footer>
   )
 }
