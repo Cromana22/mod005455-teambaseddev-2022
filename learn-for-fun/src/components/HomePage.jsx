e7b297c7e7b7f61f58a4cc093d07bbfcf654c430
Emiliana Emberton <p110132937@student.peterborough.ac.uk>
2022-11-20 11:37:27 +0000

    Edited the cards content and titles

--- learn-for-fun/src/components/HomePage.jsx
+++ learn-for-fun/src/components/HomePage.jsx
@@ -23,9 +23,9 @@ function HomePage() {
                 <div className="thirds">
                     <Card style={{ width: '20rem' }} className="homeCards">
                         <Card.Body>
-                            <Card.Title className="center">Card Title</Card.Title>
+                            <Card.Title className="center">Welcome</Card.Title>
                             <Card.Text className="justify">
-                                Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.
+                            Learn For Fun is an environment aimed at beginners. If you want a simple game to unwind or kill some time, this is the place for you. If you want to dip your proverbial toes into developing some games, this also is a good place for you. If you're just browsing aimlessly, this is still a good place to hang out.
                             </Card.Text>
                         </Card.Body>
                     </Card>
@@ -33,9 +33,9 @@ function HomePage() {
                 <div className="thirds">
                     <Card style={{ width: '20rem' }} className="homeCards">
                         <Card.Body>
-                            <Card.Title className="center">Card Title</Card.Title>
+                            <Card.Title className="center">Learn For Fun</Card.Title>
                             <Card.Text className="justify">
-                                Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.
+                                The team curated a selection of classic games to get you started, we hope you find this selection pleasing, we have more on the pipeline, please look forward to the new additions. If the selection is not to your liking, please feel free to suggest other genres, feedback is always welcomed and appreciated.
                             </Card.Text>
                         </Card.Body>
                     </Card>
@@ -43,9 +43,9 @@ function HomePage() {
                 <div className="thirds">
                     <Card style={{ width: '20rem' }} className="homeCards">
                         <Card.Body>
-                            <Card.Title className="center">Card Title</Card.Title>
+                            <Card.Title className="center">The Team</Card.Title>
                             <Card.Text className="justify">
-                                Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.
+                                This is a project part of UCP Level 6 Computing and Information Systems module, all the contributors are students, ages ranging from the early-twenties to mid-forties, a mixture of nationalities and an eclectic mix of skills and abilities. The Team Based Development cohort is grateful for your patronage.   
                             </Card.Text>
                         </Card.Body>
                     </Card>
